ae92463a6bc383a5256a663a0f87063779066033
gxnie <134279752+gxnie@users.noreply.github.com>
2024-08-08 12:53:37 +0900

    Title: 길이 단위 변환하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 240808/길이 단위 변환하기/change-length-unit.java
+++ 240808/길이 단위 변환하기/change-length-unit.java
@@ -1,20 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // 1피트와 9.2피트 변수
+        // 9.2피트 변수
         double a = 9.2;
-        double b = 30.48;
-        double ab = a * b;
 
-        System.out.print(a + "ft" + " = ");
-        System.out.printf("%.1fcm",ab);
-        System.out.print("\n");
-
-        // 1마일과 1.3마일 변수
+        // 1.3마일 변수
         double c = 1.3;
-        int d = 160934;
-        double cd = (double)c * d;
 
-        System.out.print(c + "mi" + " = ");
-        System.out.printf("%.1fcm",cd);
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm",a,a*30.48,c,c*160934);
+
     }
 }
