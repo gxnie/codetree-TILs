4afe7ab7264f6b7ff084cef79f9f52b080a510b7
gxnie <134279752+gxnie@users.noreply.github.com>
2024-08-17 14:34:36 +0900

    Title: 최대 2번의 연산, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240817/최대 2번의 연산/up-to-2-calculations.java
+++ 240817/최대 2번의 연산/up-to-2-calculations.java
@@ -10,7 +10,9 @@ public class Main {
         }
         
         if ( a % 2 != 0){
-            System.out.print((a+1)/2);
+            a =(a+1)/2;
         }
+        
+        System.out.print(a);
     }
 }
