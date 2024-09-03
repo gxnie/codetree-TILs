e7c5b4f05cba90f74dc4d8af08a7196831418d47
gxnie <134279752+gxnie@users.noreply.github.com>
2024-09-03 21:32:40 +0900

    Title: 좀 더 어려운 수학 점수, Time: 106ms, Memory: 10MB, Status: Passed - CodeTree

--- 240903/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
+++ 240903/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int aEng = sc.nextInt();
         int aMath = sc.nextInt();
+        int aEng = sc.nextInt();
 
-        int bEng = sc.nextInt();
         int bMath = sc.nextInt();
+        int bEng = sc.nextInt();
 
         if ( aMath > bMath || (aMath == bMath) && (aEng > bEng)){
             System.out.print("A");
