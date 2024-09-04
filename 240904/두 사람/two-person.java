4b208ff703aea6709783b8d896d2fa294a344ee8
gxnie <134279752+gxnie@users.noreply.github.com>
2024-09-04 21:01:37 +0900

    Title: 두 사람, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240904/두 사람/two-person.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a_age = sc.nextInt();
+        String a_gender = sc.next();
+
+        int b_age = sc.nextInt();
+        String b_gender = sc.next();
+
+        String man = "M";
+
+        if(((a_age >= 19) || ( b_age >= 19))&&((a_gender.equals(man)) || (b_gender.equals(man)))){
+            System.out.print("1");
+        } else {
+            System.out.print("0");
+        }
+    }
+}
