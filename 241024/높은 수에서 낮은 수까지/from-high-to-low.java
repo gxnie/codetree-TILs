a8fc48f07a3a5efd11106f9f62bbd317c0db67ba
gxnie <134279752+gxnie@users.noreply.github.com>
2024-10-24 09:38:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 241024/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -6,8 +6,15 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i = b; i >= a; i--){
+        if(a < b){
+            for(int i = b; i >= a; i--){
             System.out.print(i+" ");
+            }
+        } else {
+            for(int i = a; i >= b; i--){
+            System.out.print(i+" ");
+            }
         }
+        
     }
 }
