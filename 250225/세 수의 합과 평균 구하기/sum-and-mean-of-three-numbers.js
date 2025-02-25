387ca8ce41d046679932ab00257ba42f9edb6508
Genie <134279752+gxnie@users.noreply.github.com>
2025-02-25 17:12:24 +0900

    Title: 세 수의 합과 평균 구하기, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250225/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
+++ 250225/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -5,5 +5,7 @@ let a = Number(arr[0]);
 let b = Number(arr[1]);
 let c = Number(arr[2]);
 
+let total = (a+b+c)/3;
+
 console.log(a+b+c);
-console.log(((a+b+c)/3).toFixed(0));
+console.log(total.toFixed(0));
