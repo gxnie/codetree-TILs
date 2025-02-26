9433c5a38af31e1d525b252f96b1af389ce977fa
Genie <134279752+gxnie@users.noreply.github.com>
2025-02-27 01:55:10 +0900

    Title: 큰 수에서 빼기, Time: 198ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250226/큰 수에서 빼기/subtract-from-large-number.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+let arr = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+if(a >= b){
+    console.log(a-b);
+} else {
+    console.log(b-a);
+}
