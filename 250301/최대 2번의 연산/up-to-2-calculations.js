1df8c02f618adbe7f738d6c8f5b0ee7e523e9ec0
Genie <134279752+gxnie@users.noreply.github.com>
2025-03-01 14:51:43 +0900

    Title: 최대 2번의 연산, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 250301/최대 2번의 연산/up-to-2-calculations.js
+++ 250301/최대 2번의 연산/up-to-2-calculations.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 let arr = Number(fs.readFileSync(0).toString());
 
 if(arr % 2 == 0){
-    arr = parseInt(arr / 2);
+    arr /= 2;
 }
 
 if(arr % 2 == 1){
-    arr = parseInt((arr+1)/2);
+    arr += 1;
+    arr /= 2;
 }
 
 console.log(arr);
