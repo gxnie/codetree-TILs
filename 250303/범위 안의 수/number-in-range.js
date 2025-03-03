78e6ebb02248b1209a0a3d887bfacb2b09e5e5fb
Genie <134279752+gxnie@users.noreply.github.com>
2025-03-03 21:23:33 +0900

    Title: 범위 안의 수, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

--- 250303/범위 안의 수/number-in-range.js
+++ 250303/범위 안의 수/number-in-range.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 let arr = Number(fs.readFileSync(0).toString());
 
-if(arr >= 10 && arr < 20){
+if(arr >= 10 && arr <= 20){
     console.log("yes");
 } else {
     console.log("no");
