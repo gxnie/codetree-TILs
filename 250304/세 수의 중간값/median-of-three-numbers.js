5372d14bacc40d112dfe2b9a39c8bfa34863c78e
Genie <134279752+gxnie@users.noreply.github.com>
2025-03-04 22:31:46 +0900

    Title: 세 수의 중간값, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250304/세 수의 중간값/median-of-three-numbers.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let arr = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+let c = Number(arr[2]);
+
+if(b > a && b < c){
+    console.log(1);
+} else {
+    console.log(0);
+}
