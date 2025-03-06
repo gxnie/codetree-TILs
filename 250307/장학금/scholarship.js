b1fc87cffe5ccf4100af97826755d12cbd37d4a6
Genie <134279752+gxnie@users.noreply.github.com>
2025-03-07 00:03:04 +0900

    Title: 장학금, Time: 203ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250307/장학금/scholarship.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let middleScore = Number(input[0]);
+let finalScore = Number(input[1]);
+
+if (middleScore >= 90 && finalScore >= 95) {
+    console.log(100000);
+}
+else if (middleScore >= 90 && finalScore >= 90) {
+    console.log(50000)
+}
+else {
+    console.log(0);
+}
