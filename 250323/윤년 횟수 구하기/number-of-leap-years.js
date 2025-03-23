dfd9bd34e3fa5de1554fafbdaedb27de2e97f45d
Genie <134279752+gxnie@users.noreply.github.com>
2025-03-23 18:41:56 +0900

    Title: 윤년 횟수 구하기, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 250323/윤년 횟수 구하기/number-of-leap-years.js
+++ 250323/윤년 횟수 구하기/number-of-leap-years.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 let arr = Number(fs.readFileSync(0).toString().trim());
 let cnt = 0;
 
-for(let i = 1; i < arr; i++){
+for(let i = 1; i <= arr; i++){
    if((i % 4 === 0) && (i % 100 != 0)){
       cnt++;
    }
