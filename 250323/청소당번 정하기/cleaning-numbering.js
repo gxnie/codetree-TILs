1b55467a58364befb23ab4ab033984eb2eff7d0b
Genie <134279752+gxnie@users.noreply.github.com>
2025-03-23 18:04:03 +0900

    Title: 청소당번 정하기, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250323/청소당번 정하기/cleaning-numbering.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+let arr = Number(fs.readFileSync(0).toString().trim());
+let cntA = 0, cntB = 0, cntC = 0;
+
+for(let i = 1; i <= arr; i++){
+   if(i % 12 === 0){
+    cntC++;
+   } else if ( i % 3 === 0){
+    cntB++;
+   } else if ( i % 2 === 0){
+    cntA++;
+   }
+}
+
+console.log(cntA,cntB,cntC);
